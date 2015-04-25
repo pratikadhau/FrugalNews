6129395e7deb26f5f64003c2dcefcc08a99f5c6d
pratik adhau <pratikadhau1@gmail.com>
2015-04-25 20:38:25 +0530

    Removed all news from home page

--- src/main/webapp/modules/home/HomeController.js
+++ src/main/webapp/modules/home/HomeController.js
@@ -5,13 +5,16 @@ homeApp.controller("HomeController", [
 		function($scope, $rootScope, $http) {
 			_readNews = {};
 			$scope.readNews ={};
-			$scope.activeTab = 'home'
+			$scope.activeTab = 'Headline'
 			$scope.readNews ={};
 			$scope.summaryView = true;
 			$http.get($rootScope.baseUrl + "/api/news").success(function(data) {
-				$scope.newsList = data;
-				$scope.newsList.forEach(function(e,i,a) {
-					$scope.readNews[e.newsId]=false
+				$scope.newsList = []
+				$scope.allNews = data;
+				$scope.allNews.forEach(function(e,i,a) {
+					$scope.readNews[e.newsId]=false;
+					if(e.newsType == 'Headline')
+					$scope.newsList.push(e);
 				})
 			});
 
@@ -28,27 +31,19 @@ homeApp.controller("HomeController", [
 			}
 			
 			$scope.showsNews = function(newsType) {
-				$http.get($rootScope.baseUrl + "/api/news/types/" + newsType)
-						.success(function(data) {
-							$scope.newsList = data;
-							$scope.selectedNews = "";
-							$scope.activeTab = newsType
-						});
+				var _allNews = $scope.allNews;
+				var _filteredNews = [];
+				_allNews.forEach(function(e,i,a) {
+					if(e.newsType == newsType){
+						_filteredNews.push(e);
+					}
+				});	
+				$scope.newsList = _filteredNews;
+				$scope.selectedNews = "";
+				$scope.activeTab = newsType
 				$scope.summaryView = true;
 			}
 
-			$scope.showsAllNews = function() {
-				$http.get($rootScope.baseUrl + "/api/news").success(
-						function(data) {
-							$scope.newsList = data;
-							$scope.newsList.forEach(function(e,i,a) {
-								$scope.readNews[e.newsId]=false
-							})
-							$scope.selectedNews = "";
-							$scope.activeTab = "home";
-				});
-				$scope.summaryView = true;
-			};
 			
 			$scope.isActive = function (newsType){
 				return $scope.activeTab == newsType ? 'active' : '';
